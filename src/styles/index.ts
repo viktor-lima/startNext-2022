f2042fafddfeedc3cb24a2359715e920a130d371
viktor-lima <joaolimak10@outlook.com>
2025-11-21 11:46:25 -0300

    ft: global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -11,8 +11,15 @@ export const {
   createTheme
 } = createStitches({
   theme: {
-   colors: {
-    rocketseat: '#8257e6'
-   }  
+    colors: {
+      white: '#fff',
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
+    }
   }
 })
