34fc2da69ac7317b243c170ac414a7428bd6435f
viktor-lima <joaolimak10@outlook.com>
2025-11-22 16:37:35 -0300

    ft: home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
+import Image from "next/image"
 import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    backgroundColor: 'MediumPurple'
-  },
-})
+import camiseta1 from '../assets/Shirt/1.png'
+import camiseta2 from '../assets/Shirt/2.png'
+import camiseta3 from '../assets/Shirt/3.png'
+import camiseta4 from '../assets/Shirt/4.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt=""/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
