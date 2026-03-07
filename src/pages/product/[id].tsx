023ab64fb88060c6aa5fc8c0daa8808c10257fa7
viktor-lima <joaolimak10@outlook.com>
2026-03-07 10:44:41 -0300

    ft: structure product details page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "../../../node_modules/next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 
 interface productProps {
@@ -10,8 +11,20 @@ export default function Product(props: productProps) {
   const { query } = useRouter()
 
   return (
-    <h1>
-      Product: {JSON.stringify(query)}
-    </h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda sit porro quis ab dolorum asperiores non doloribus, veniam facilis repellat maxime, odit vel alias ut possimus dicta optio hic accusantium.</p>
+      
+        <button>Comprar agora</button>
+
+      </ProductDetails>
+    </ProductContainer>
   );
 }
